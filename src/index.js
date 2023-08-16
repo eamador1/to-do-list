0da2e6ff6158700d262429c5b90a2824b280156c
erwin amador <eamador0305@gmail.com>
2023-08-16 16:31:54 -0600

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -22,24 +22,23 @@ class Goals {
     $list.innerHTML = '';
 
     this.items.forEach((item) => {
-      const $cont = document.createElement('div')
+      const $cont = document.createElement('div');
       const $checkbox = document.createElement('input');
       $checkbox.type = 'checkbox';
       const $task = document.createElement('span');
-      const $dots = document.createElement('img')
+      const $dots = document.createElement('img');
 
-      $cont.classList.add('cont-task')
+      $cont.classList.add('cont-task');
       $checkbox.classList.add('completed');
       $task.classList.add('task');
       $dots.classList.add('dots');
-      
 
       $task.textContent = item.description;
       $dots.src = 'src/img/dots.png';
       $dots.alt = 'dots image';
 
       $cont.appendChild($checkbox);
-      $cont.appendChild($task); 
+      $cont.appendChild($task);
       $cont.appendChild($dots);
       $list.appendChild($cont);
     });
