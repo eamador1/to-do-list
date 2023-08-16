ebfd65e421764362fcfb9fd2e421b9b46a606b69
erwin amador <eamador0305@gmail.com>
2023-08-16 16:26:40 -0600

    Display list, insert images

--- src/index.js
+++ src/index.js
@@ -22,17 +22,26 @@ class Goals {
     $list.innerHTML = '';
 
     this.items.forEach((item) => {
+      const $cont = document.createElement('div')
       const $checkbox = document.createElement('input');
       $checkbox.type = 'checkbox';
       const $task = document.createElement('span');
+      const $dots = document.createElement('img')
 
-      $task.classList.add('task');
+      $cont.classList.add('cont-task')
       $checkbox.classList.add('completed');
+      $task.classList.add('task');
+      $dots.classList.add('dots');
+      
 
       $task.textContent = item.description;
+      $dots.src = 'src/img/dots.png';
+      $dots.alt = 'dots image';
 
-      $list.appendChild($task);
-      $list.appendChild($checkbox);
+      $cont.appendChild($checkbox);
+      $cont.appendChild($task); 
+      $cont.appendChild($dots);
+      $list.appendChild($cont);
     });
   }
 }
